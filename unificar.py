690766af83c9b7463f838a5ef123a555f2dcce37
yojuancarlos <56267375+yojuancarlos@users.noreply.github.com>
2022-07-21 10:46:37 -0500

    agregue comentarios al codigo

--- unificar.py
+++ unificar.py
@@ -4,15 +4,15 @@ import os
 
 
 clase_parametros = parametros.Parametros()
-
+# aqui uno todas las validaciones en una sola y utilizo la tabla de verdad de conjuncion
 def validaciones_totales():
    if valida() and valida2() and valida3():
        return True
    else: return False
-
+# aqui parseo los argumentos para ser usado en otras clases
 def parseo():
     clase_parametros.argumentos = clase_parametros.opciones.parse_args()
-
+# esta es la primera validacion (la carpeta 1 y 2 tienen que ser directorios,la ruta de salida no puede existir la carpeta 1 y 2 tienen que tener al menos 1 archivo)
 def valida():
     respuesta = True
     parseo()
@@ -22,18 +22,22 @@ def valida():
                  and validaciones.carpeta_tiene_archivos(clase_parametros.argumentos.dir1)
                  and validaciones.carpeta_tiene_archivos(clase_parametros.argumentos.dir2))
     return respuesta
+#esta es la validacion2 se debe seleccionar almenos una de las opciones
 def valida2():
     respuesta = True
     parseo()
     respuesta = (clase_parametros.argumentos.modificaton_date or clase_parametros.argumentos.size
                  or clase_parametros.argumentos.fingerprint)
     return respuesta
+#esta el la validacion 3 la carpeta 1 debe ser diferente de la 2
 def valida3():
     respuesta = True
     parseo()
     if clase_parametros.argumentos.dir1 == clase_parametros.argumentos.dir2:
         respuesta=False
     return respuesta
+
+#si todas las validaciones se cumplen se puede imprimir el mensaje
 def print_argst():
    if validaciones_totales():
       directory1 = clase_parametros.argumentos.dir1
