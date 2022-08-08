9d03bfcfdae342ada54455b9fb345c0b5a4bdd1f
yojuancarlos <56267375+yojuancarlos@users.noreply.github.com>
2022-08-08 15:46:15 -0500

    logre craer el dicionario de segundo nivel con el archivo y su ruta

--- directorio.py
+++ directorio.py
@@ -7,19 +7,33 @@ class directorio:
         self.directorio1 = directorio
 
     def diccionarioderutas(self,directorio):
+        rutas=[]
+        rutasyarchivos=[]
+        rutasyarchivos2 = []
+        diccionario_de_respuesta = {}
         for root, dirs, files in os.walk(directorio, topdown=True):
-
             for file in files:
-                print(file)
-                print(f"{os.path.join(root,file)}")
-                print(type(os.path.join(root,file)))
+                rutasyarchivos=os.path.join(root,file)
+                rutas.append((file,rutasyarchivos))
+                #print(f"{os.path.join(root,file)}")
+               # print(type(os.path.join(root,file)))
 
-    def unirlistas(self):
-        lista1=self.diccionarioderutas("C:\pruebas\carpeta1")
-        lista2=self.diccionarioderutas("C:\pruebas\carpeta2")
+        diccionario_de_respuesta['archivosysuruta'] = {}
+        for elemento in rutas:
+            key = elemento[0]
+            value = elemento[1]
+            diccionario_de_respuesta['archivosysuruta'][key] = value
 
-
+        print(diccionario_de_respuesta)
 """""
+diccionario_de_respuesta['archivosconmayorpeso']={}
+for elemento in rutasypeso:
+    key=elemento[0]
+    value=elemento[1]
+    diccionario_de_respuesta['archivosconmayorpeso'][key]=value
+
+
+
         diccionario_de_respuesta['archivosconmayorpeso']={}
 for elemento in rutasypeso:
     key=elemento[0]
@@ -34,17 +48,6 @@ for root,dirs,files in os.walk(directorio, topdown=True):
         print (files) 
         print ('--------------------------------') 
 
-Este método crea un nuevo diccionario con claves a partir de un tipo de dato secuencia. El valor de value por defecto es el tipo None.
-
->>> secuencia = ('python', 'zope', 'plone')
->>> versiones = dict.fromkeys(secuencia)
->>> print "Nuevo Diccionario : %s" %  str(versiones)
-Nuevo Diccionario : {'python': None, 'zope': None, 'plone': None}
-En el ejemplo anterior inicializa los valores de cada clave a None, mas puede inicializar un valor común por defecto para cada clave:
-
->>> versiones = dict.fromkeys(secuencia, 0.1)
->>> print "Nuevo Diccionario : %s" %  str(versiones)
-Nuevo Diccionario : {'python': 0.1, 'zope': 0.1, 'plone': 0.1}
 """
 
 direc=directorio()
