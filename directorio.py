e316ce90908d886bb6dee37f50021bdab9ec9870
yojuancarlos <56267375+yojuancarlos@users.noreply.github.com>
2022-08-09 16:59:33 -0500

    corregi el diccionario y comienzo a meter docstraings a todas las funciones

--- directorio.py
+++ directorio.py
@@ -7,6 +7,31 @@ class directorio:
         self.directorio1 = directorio
 
     def diccionarioderutas(self,directorio):
+        """""
+        esta funcion sirve para crear una lista con los nombres de todos los archivos que se encuentren en un directorio
+        despues crea un dicciorio el cual cada nombre de archivo contenga su ruta
+        el diccionario debe contener esta estructura :
+
+        {
+	"01010000.dmx": {
+		"D:/carpeta1":{
+			"ruta": "D:/carpeta1",
+			"tamano": 300
+		},
+		"Z:/carpeta_dmx":{
+			"ruta": "Z:/carpeta_dmx/2022/01/01",
+			"tamano": 72000
+		}
+	},
+	"01010001.dmx": {
+		"D:/carpeta1":{
+			"ruta": "D:/carpeta1",
+			"tamano": 300
+		},
+
+
+        :param
+        """
         rutas=[]
         rutasyarchivos=[]
         rutasyarchivos2 = []
@@ -17,22 +42,16 @@ class directorio:
                 rutas.append((file,rutasyarchivos))
                 #print(f"{os.path.join(root,file)}")
                # print(type(os.path.join(root,file)))
-
-        diccionario_de_respuesta['archivosysuruta'] = {}
         for elemento in rutas:
             key = elemento[0]
             value = elemento[1]
-            diccionario_de_respuesta['archivosysuruta'][key] = value
+            diccionario_de_respuesta[key] = {}
+            diccionario_de_respuesta[key][value] = value
+            diccionario_de_respuesta[key][value] = {}
 
         print(diccionario_de_respuesta)
-"""""
-for root,dirs,files in os.walk(directorio, topdown=True): 
-        print (root) 
-        print (dirs) 
-        print (files) 
-        print ('--------------------------------') 
 
-"""
 
 direc=directorio()
 direc.diccionarioderutas("C:\pruebas\carpeta1")
+help(direc)
